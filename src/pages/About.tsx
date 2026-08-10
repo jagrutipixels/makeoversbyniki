4c0aad11b874a62c1e34c9e81206a3a441a7f963
jagrutipixels <jagrutipixels@gmail.com>
2026-08-10 17:23:30 +0530

    Synchronize Niki eye makeup photo (1n1Sefn5Ahl0tmbw2j956nayR3W_uEPwW) across Signature Experience and About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -24,7 +24,7 @@ export default function About() {
           className="lg:col-span-5 relative aspect-[4/5] sm:aspect-[3/4] rounded-sm overflow-hidden shadow-2xl border border-white/10"
         >
           <img 
-            src="https://drive.google.com/thumbnail?id=1PSFKJbo2m3f_mrHuifIU3iKa5WF-sNLW&sz=w1080" 
+            src="https://drive.google.com/thumbnail?id=1n1Sefn5Ahl0tmbw2j956nayR3W_uEPwW&sz=w1080" 
             alt="Niki working" 
             className="w-full h-full object-cover object-[center_top]"
             referrerPolicy="no-referrer"
