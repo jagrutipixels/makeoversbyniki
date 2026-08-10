e83bcb0e0b30d6b0192e5d04da0f4a681aa77d64
jagrutipixels <jagrutipixels@gmail.com>
2026-08-10 15:37:19 +0530

    Shift mobile focal points upwards for Categories 2, 4, and 5 for optimal framing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -45,7 +45,7 @@ const CATEGORIES = [
     landscape: "https://drive.google.com/thumbnail?id=1TkYZRkx6Bft1SKiE1LvKH4c3CwZdTNVo&sz=w800",
     mobile: "https://drive.google.com/thumbnail?id=1MN7DdCWXD-xdkkJm6yonq0Sba2ZHt9BJ&sz=w1080",
     position: "object-[center_top]",
-    mobilePosition: "object-[center_top]"
+    mobilePosition: "object-[center_35%]"
   },
   { 
     title: "Airbrush Bridal Makeup", 
@@ -59,14 +59,14 @@ const CATEGORIES = [
     landscape: "https://drive.google.com/thumbnail?id=1j1jVNPVbMysukBukRXcxOTYKs2NwDgNg&sz=w800",
     mobile: "https://drive.google.com/thumbnail?id=1kUqsH-Fh-XD9HAXhv0A6Dabt5tIP377d&sz=w1080",
     position: "object-center",
-    mobilePosition: "object-[center_top]"
+    mobilePosition: "object-[center_35%]"
   },
   { 
     title: "Destination Bridal Experience", 
     landscape: "https://drive.google.com/thumbnail?id=1sl2XSexbz5oJvsiHNS5ClvZikh3vIu_g&sz=w800",
     mobile: "https://drive.google.com/thumbnail?id=14kKbtmD8rU-oJzjnQm_o44E0Z8RL5zBg&sz=w1080",
     position: "object-[30%_center]",
-    mobilePosition: "object-[center_top]"
+    mobilePosition: "object-[center_35%]"
   }
 ];
 
