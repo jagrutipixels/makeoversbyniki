4f150a6b15ef7e6bb8b187f93b62b82d7410e1d0
jagrutipixels <jagrutipixels@gmail.com>
2026-08-10 12:27:04 +0530

    Fix React namespace import in Home.tsx touch event handlers

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { motion, AnimatePresence } from 'motion/react';
-import { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { IMAGES } from '../data';
 import { ChevronLeft, ChevronRight, X as CloseIcon, Instagram, Facebook, Twitter } from 'lucide-react';
